bda2112db710a86b493565381bdba80133a8c89c
Sunpreet Singh <114453652+sun-28@users.noreply.github.com>
2024-11-30 15:37:38 +0530

    small fix

--- middlewares/roleMiddleware.js
+++ middlewares/roleMiddleware.js
@@ -1,6 +1,6 @@
 const roleCheck = (roles) => {
   return (req, res, next) => {
-    const userRole = req.user?.role;
+    const userRole = req.session.user?.role;
     if (!userRole || !roles.includes(userRole)) {
       return res.status(403).send({ message: "Forbidden: Insufficient Role" });
     }
